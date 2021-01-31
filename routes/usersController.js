7172a65878485a0ec5998a0d038aeb8a36897d68
MrMarrant <georgesandalous@gmail.com>
2021-01-31 20:25:46 +0100

    test provisoire users controller

--- routes/usersController.js
+++ routes/usersController.js
@@ -20,7 +20,7 @@ module.exports = {
         var mysql = require('mysql');
 
 var con = mysql.createConnection({
-  host: "https://phpmyadmin.cluster028.hosting.ovh.net/",
+  host: "https://phpmyadmin.cluster028.hosting.ovh.net/db_structure.php?server=1&db=rireceeadmin",
   user: "rireceeadmin",
   password: "Alouat50trin",
   database: "rireceeadmin"
